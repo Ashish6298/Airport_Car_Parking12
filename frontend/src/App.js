afa1309e2c2a806cef1c88ebef0fa720c8938241
Ashish Goswami <ashishgoswami1013@gmail.com>
2023-03-16 21:52:45 +0530

    step-8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,22 @@
 import './style.css';
+import HomePage from './pages/Home';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Layout from './pages/Layout';
-import HomePage from './pages/HomePage';
+import Layout from './components/Layout';
 import Login from './pages/Login';
-import AirportAvailability from './pages/AirportAvailability';
+import AirportAvailability from './pages/AirportAvailability'
+
 function App() {
   return (
-   <BrowserRouter>
-   <Routes>
-   <Route path="/" element={<Layout/>}>
-   <Route path="/" index element={<HomePage/>}/>
-   <Route path="/login" element={<Login/>}/>
-   <Route path="*" element={<HomePage/>}/>
-   <Route path="results" element={<AirportAvailability/>} />
-   </Route>
-   </Routes>
-   </BrowserRouter>
+    <BrowserRouter >
+    <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />
+      <Route path="results" element={<AirportAvailability />} />
+      <Route path="login" element={<Login />} />
+      <Route path="*" element={<HomePage />} />
+    </Route>
+  </Routes>
+  </BrowserRouter >
   );
 }
 
